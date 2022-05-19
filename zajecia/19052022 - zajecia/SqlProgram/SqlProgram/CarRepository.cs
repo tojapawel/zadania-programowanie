716a2365774bfbf701d8af1dd0c0b9a28f5f7e99
Paweł <39226176+tojapawel@users.noreply.github.com>
2022-05-19 19:36:54 +0200

    190522

---
+++ zajecia/19052022 - zajecia/SqlProgram/SqlProgram/CarRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.Sqlite;
+
+internal class CarRepository {
+
+    private SqliteConnection _sqliteConnection = new SqliteConnection();
+
+    public void Open(string connetcionString) {
+        _sqliteConnection.ConnectionString = connetcionString;
+        _sqliteConnection.Open();
+    }
+
+    public void Close() { 
+        _sqliteConnection.Close();
+    }
+
+    public void AddNewCar(string brand, string petName, string color) {
+        var sqlQuerry = $"INSERT INTO car (car_id, brand, petName, color) VALUES (null, '{brand}', '{petName}', '{color}')";
+
+        using (var command = new SqliteCommand(sqlQuerry, _sqliteConnection)) {
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public void DeleteCar(int car_id) {
+        var sqlQuerry = $"DELETE FROM car WHERE `car_id` = '{car_id}'";
+
+        using (var command = new SqliteCommand(sqlQuerry, _sqliteConnection)) {
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public void UpdateCar(int car_id, string brand, string petName, string color) {
+        var sqlQuerry = $"UPDATE car SET `brand` = '{brand}', `petName` = '{petName}', `color` = '{color}' WHERE `car_id` = '{car_id}'";
+
+        using (var command = new SqliteCommand(sqlQuerry, _sqliteConnection)) {
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public void ShowCars() {
+        var sqlQuerry = "SELECT * FROM car";
+
+        using var cmd = new SqliteCommand(sqlQuerry, _sqliteConnection);
+        using SqliteDataReader rdr = cmd.ExecuteReader();
+
+        while (rdr.Read()) {
+            Console.WriteLine($"\nid: {rdr.GetInt32(0)}\nbrand: {rdr.GetString(1)}\npetName: {rdr.GetString(2)}\ncolor: {rdr.GetString(3)}\n");
+        }
+    }
+
+}
---
+++ zajecia/19052022 - zajecia/SqlProgram/SqlProgram/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Configuration;
+
+var myConnString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+var carRepo = new CarRepository();
+carRepo.Open(myConnString);
+
+
+while (true) {
+    DisplayInfo();
+    Console.Write("Wybierz: ");
+    var res = int.Parse(Console.ReadLine());
+
+    switch (res) {
+        case 1:
+            InsertNewCar();
+            break;
+        case 2:
+            RemoveCar();
+            break;
+        case 3:
+            UpdateCar();
+            break;
+        case 4:
+            ShowCars();
+            break;
+        default:
+            Console.WriteLine("Blad");
+            break;
+    }
+}
+
+carRepo.Close();
+
+void RemoveCar() {
+    Console.Clear();
+    Console.WriteLine("Usuwanie samochodu.");
+
+    Console.Write("Podaj id samochodu do usuniecia: ");
+    var car_id = int.Parse(Console.ReadLine());
+
+
+    //usuwanie z bazy danych
+
+    carRepo.DeleteCar(car_id);
+}
+
+void InsertNewCar() {
+    Console.Clear();
+    Console.WriteLine("Dodawanie nowego samochodu.");
+
+    Console.Write("Podaj brand: ");
+    var brand = Console.ReadLine();
+
+    Console.Write("Podaj petName: ");
+    var pet_name = Console.ReadLine();
+
+    Console.Write("Podaj color: ");
+    var color = Console.ReadLine();
+
+
+    //dodawanie do bazy danych
+
+    carRepo.AddNewCar(brand, pet_name, color);
+
+}
+
+void UpdateCar() {
+    Console.Clear();
+    Console.WriteLine("Edytowanie samochodu.");
+
+    Console.Write("Podaj id samochodu do edycji: ");
+    var car_id = int.Parse(Console.ReadLine());
+
+    Console.Write("Podaj nowy brand: ");
+    var brand = Console.ReadLine();
+
+    Console.Write("Podaj nowy petName: ");
+    var pet_name = Console.ReadLine();
+
+    Console.Write("Podaj nowy color: ");
+    var color = Console.ReadLine();
+
+
+    //update do bazy danych
+
+    carRepo.UpdateCar(car_id, brand, pet_name, color);
+
+}
+
+void ShowCars() {
+    Console.Clear();
+    Console.WriteLine("Samochody:");
+
+    carRepo.ShowCars();
+    Console.Write("Kliknij cokolwiek aby kontynuowac: ");
+    Console.ReadKey();
+}
+
+void DisplayInfo() {
+    Console.Clear();
+    Console.WriteLine("Opcje");
+    Console.WriteLine("1. Dodaj");
+    Console.WriteLine("2. Usun");
+    Console.WriteLine("3. Edytuj");
+    Console.WriteLine("4. Wyswietl");
+}

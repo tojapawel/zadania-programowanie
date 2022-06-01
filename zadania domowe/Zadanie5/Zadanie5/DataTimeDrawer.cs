9a9700c35c9418f267929fe43fcbd2879e0fb175
Paweł <pawel.kozicki01@gmail.com>
2022-06-01 19:53:05 +0200

    zadanie domowe nr 5

---
+++ zadania domowe/Zadanie5/Zadanie5/DataTimeDrawer.cs
@@ -0,0 +1,30 @@
+﻿public class DataTimeDrawer {
+    private DateTime _startDate;
+    private DateTime _endDate;
+
+    public DataTimeDrawer(DateTime startDate, DateTime endDate) {
+        _startDate = startDate;
+        _endDate = endDate;
+    }
+
+    public void DrawDate() {
+        int dayCount = (int)(_endDate - _startDate).TotalDays;
+
+        var currDate = _startDate;
+
+        DateTime[] daysDateTime = new DateTime[dayCount];
+        string[] days = new string[dayCount];
+
+
+        for (int i = 0; i < dayCount; i++) {
+            daysDateTime[i] = currDate;
+            days[i] = daysDateTime[i].ToShortDateString();
+
+            currDate = currDate.AddDays(1);
+        }
+
+        foreach (var item in days) {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ zadania domowe/Zadanie5/Zadanie5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Podaj date poczatkowa (YYYY,MM,DD): ");
+var startDateString = Console.ReadLine();
+
+Console.Write("Podaj date koncowa (YYYY,MM,DD): ");
+var endDateString = Console.ReadLine();
+
+int[] startDate = new int[3];
+int[] endDate = new int[3];
+
+for (int i = 0; i < 3; i++) {
+    startDate[i] = int.Parse(startDateString.Split('\u002C')[i]);
+    endDate[i] = int.Parse(endDateString.Split('\u002C')[i]);
+}
+
+var date = new DataTimeDrawer(new DateTime(startDate[0], startDate[1], startDate[2]), new DateTime(endDate[0], endDate[1], endDate[2]));
+
+date.DrawDate();

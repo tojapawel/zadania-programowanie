6977c036bdbd30cd10118916012003b348279038
Paweł <39226176+tojapawel@users.noreply.github.com>
2022-06-02 17:50:38 +0200

    02-06-2022 2

--- zajecia/02-06-2022/ConsoleApp1/ConsoleApp1/Program.cs
+++ zajecia/02-06-2022/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,18 @@
-﻿var employees = GetAllEmployeesDb();
+﻿using System.Collections;
+
+var employees = GetAllEmployeesDb();
 Console.WriteLine(new string('-', 30));
 foreach (var item in employees) {
-    Console.WriteLine(item);
-}
-
-static Employee[] GetAllEmployeesDb() {
-    var lenght = 100;
-    var array = new Employee[lenght];
-
-    for (int i = 0; i < lenght; i++) {
-        Console.WriteLine("-> " + i);
-        array[i] = new Employee(i+100, "Test " + i);
+    if (((Employee)item).ID < 115) {
+        Console.WriteLine(item);
+    } else {
+        break;
     }
-    return array;
 }
 
+static IEnumerable GetAllEmployeesDb() {
+    for (int i = 0; i < 100; i++) {
+        yield return new Employee(i + 100, "Test " + i);
+        Console.WriteLine("-> " + i);
+    }
+}

a935f5ad55955bc85f126281a4286e4e266317c6
Paweł <39226176+tojapawel@users.noreply.github.com>
2022-06-02 18:00:30 +0200

    02-06-2022 3-Clone

---
+++ zajecia/02-06-2022/ConsoleApp1/ConsoleApp1/Point.cs
@@ -0,0 +1,18 @@
+﻿public class Point : ICloneable{
+    public Point(int x, int y) {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public object Clone() {
+        // lub: return new Point(X, Y);
+        return this.MemberwiseClone();
+    }
+
+    public override string ToString() {
+        return $"x: {X}, y: {Y}";
+    }
+}
--- zajecia/02-06-2022/ConsoleApp1/ConsoleApp1/Program.cs
+++ zajecia/02-06-2022/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 
-var employees = GetAllEmployeesDb();
-Console.WriteLine(new string('-', 30));
-foreach (var item in employees) {
-    if (((Employee)item).ID < 115) {
-        Console.WriteLine(item);
-    } else {
-        break;
-    }
-}
+var p1 = new Point(5, 10);
+var p2 = (Point)p1.Clone();
 
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+p2.X = 25;
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+Console.ReadLine();
+
+
+
+
+
+
+
+//IEnum();
 static IEnumerable GetAllEmployeesDb() {
     for (int i = 0; i < 100; i++) {
         yield return new Employee(i + 100, "Test " + i);
         Console.WriteLine("-> " + i);
+    }
+}
+static void IEnum() {
+    var employees = GetAllEmployeesDb();
+    Console.WriteLine(new string('-', 30));
+    foreach (var item in employees) {
+        if (((Employee)item).ID < 115) {
+            Console.WriteLine(item);
+        } else {
+            break;
+        }
     }
 }

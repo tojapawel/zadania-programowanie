aac68667c33659fe432cf02b1e4f49d2fe3f1974
Paweł <39226176+tojapawel@users.noreply.github.com>
2022-06-02 17:30:00 +0200

    02-06-2022 start

---
+++ zajecia/02-06-2022/ConsoleApp1/ConsoleApp1/Employee.cs
@@ -0,0 +1,13 @@
+﻿public class Employee {
+    public Employee(int iD, string firstName) {
+        ID = iD;
+        FirstName = firstName;
+    }
+
+    public int ID { get; }
+    public string FirstName { get; }
+
+    public override string ToString() {
+        return $"ID: {ID}, FirstName: {FirstName}";
+    }
+}
---
+++ zajecia/02-06-2022/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿var employees = GetAllEmployeesDb();
+Console.WriteLine(new string('-', 30));
+foreach (var item in employees) {
+    Console.WriteLine(item);
+}
+
+static Employee[] GetAllEmployeesDb() {
+    var lenght = 100;
+    var array = new Employee[lenght];
+
+    for (int i = 0; i < lenght; i++) {
+        Console.WriteLine("-> " + i);
+        array[i] = new Employee(i+100, "Test " + i);
+    }
+    return array;
+}
+

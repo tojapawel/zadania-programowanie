f0d02edad000dfe2e8407456bd2300b24309fe70
Paweł <39226176+tojapawel@users.noreply.github.com>
2022-06-09 17:48:23 +0200

    "zadanie domowe"

---
+++ zajecia/16-06-2022/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1 {
+    public class Periiod {
+        private DateTime date1;
+        private DateTime date2;
+
+        public Periiod(DateTime date1, DateTime date2) {
+            this.date1 = date1;
+            this.date2 = date2;
+        }
+    }
+}
---
+++ zajecia/16-06-2022/ConsoleApp1/ConsoleApp1/Period.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+public class Period : IEnumerable{
+
+    private DateTime dateTime1;
+    private DateTime dateTime2;
+
+    public Period(DateTime dateTime1, DateTime dateTime2) {
+        this.dateTime1 = dateTime1;
+        this.dateTime2 = dateTime2;
+    }
+
+    public IEnumerator GetEnumerator() {
+        for (var date = dateTime1; date <= dateTime2; date = date.AddDays(1)) {
+            yield return date;
+        }
+    }
+}
